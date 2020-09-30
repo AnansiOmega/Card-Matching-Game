380c731d80edee75c23637b21d89859089bf6917
thrlstl <matthewsteeleonline@gmail.com>
2020-09-29 20:06:40 -0500

    front end updates, toggle cards up or down

--- cardGameFrontEnd/index.js
+++ cardGameFrontEnd/index.js
@@ -1,4 +1,7 @@
 const cardGameCont = document.getElementById('card-game-container')
+const cards1 = document.getElementsByClassName('card1')
+const cards2 = document.getElementsByClassName('card2')
+const cardsDown = document.getElementsByClassName('card-down')
 const pair1 = document.querySelector('#pair-1')
 const pair2 = document.querySelector('#pair-2')
 const pair3 = document.querySelector('#pair-3')
@@ -7,6 +10,8 @@ const pair5 = document.querySelector('#pair-5')
 const pair6 = document.querySelector('#pair-6')
 const pair7 = document.querySelector('#pair-7')
 const pair8 = document.querySelector('#pair-8')
+
+pair1.addEventListener('click', toggleCardsDown)
 
 
 function main(){
@@ -30,10 +35,10 @@ function loadNewGame(){
 
 function renderNewGame(newGame){
     placeCards(newGame)
+    toggleCardsUp()
 }
 
 function placeCards(newGame) {
-
     const match1 = newGame[0].match
     const match2 = newGame[1].match
     const match3 = newGame[2].match
@@ -45,35 +50,59 @@ function placeCards(newGame) {
 
     return (
         pair1.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match1.english_word}</h4></div>
+
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match1.spanish_word}</h4></div>
         `,
         pair2.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match2.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match2.spanish_word}</h4></div>
         `,
         pair3.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match3.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match3.spanish_word}</h4></div>
         `,
         pair4.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match4.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match4.spanish_word}</h4></div>
         `,
         pair5.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match5.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match5.spanish_word}</h4></div>
         `,
         pair6.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match6.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match6.spanish_word}</h4></div>
         `,
         pair7.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match7.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match7.spanish_word}</h4></div>
         `,
         pair8.innerHTML = `
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card1'><h4>${match8.english_word}</h4></div>
+        
+        <div class='card-down'>${renderMintLeaf()}</div>
         <div class='card2'><h4>${match8.spanish_word}</h4></div>
         `
     )
@@ -84,6 +113,36 @@ function gameType(type) {
         englishSpanishGame(type)
     }
 
+}
+
+function toggleCardsUp() {
+    [...cards1].forEach(card => {
+        card.style.display = '';
+    });
+    [...cards2].forEach(card => {
+        card.style.display = '';
+    });
+    [...cardsDown].forEach(card => {
+        card.style.display = 'none';
+    })
+}
+
+function toggleCardsDown() {
+    [...cards1].forEach(card => {
+        card.style.display = 'none';
+    });
+    [...cards2].forEach(card => {
+        card.style.display = 'none';
+    });
+    [...cardsDown].forEach(card => {
+        card.style.display = '';
+    })
+}
+
+function renderMintLeaf() {
+ return `
+ <img id='mint-leaf' src="/Users/matthewsteele/Development/code/Mod3/Project/Card-Matching-Game/cardGameBackEnd/app/assets/images/mintleaf.png">
+ `
 }
 
 
